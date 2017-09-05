bfea6e26f24627d13c3698bcd21440814db864da
goldeneyezhang <goldeneyezhang@163.com>
2017-09-05 08:52:36 +0800

    第一章

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,11 @@
     <div id="app">
         <h1>{{title}}</h1>
         <ul class="todos">
-            <li>
-                <input v-model="newTodo"
-                       @keyup.13="addItem"
-                       placeholder="快写下您要我记住的事吧"
-                       autofocus="true"/>
-            </li>
+           
             <li v-for="(todo,index) in todos"
                 :class="{'checked':todo.done}">
-                <input type="checkbox"
-                       @change="saveToStore"
-                       v-model="todo.done"
-                /><label>{{ index+1 }}.{{ todo.value }}</label>
-                <time>{{ todo.created | date }}</time>
-                <button @click.prevent="delItem(todo)"></button>
+               <label>{{index+1}}.{{todo.value}}</label>
+               <time>{{todo.created|date}}</time>
             </li>
         </ul>
     </div>
@@ -27,47 +18,20 @@
     import moment from 'moment'
     import 'moment/locale/zh-cn'
     moment.locale('zh-cn')
-
     export default {
         name: 'app',
         data () {
             return {
                 newTodo:'',
                 title: 'vue-todos',
-                todos: []
+                todos: [{value:"read a book about vue2",done:false,created:moment().valueOf()},
+                {value:"supply sample code",done:true,created:moment().valueOf()+30000},
+                {value:"write a review",done:false,created:moment().valueOf()-30000000}]
             }
         },
-        created (){
-            if (this.is_initialized) {
-                this.todos = JSON.parse(localStorage.getItem('VUE-TODOS'))
-            }
-        },
-        computed: {
-            is_initialized (){
-                return localStorage.getItem('VUE-TODOS') != null
-            }
-        },
-        filters: {
+        filters:{
             date(val){
                 return moment(val).calendar()
-            }
-        },
-        methods: {
-            addItem() {
-                this.todos.push({
-                    value: this.newTodo,
-                    created: Date.now(),
-                    done: false
-                });
-                this.saveToStore();
-                this.newTodo = ''
-            },
-            delItem (todo) {
-                this.todos = this.todos.filter((x) => x !== todo)
-                this.saveToStore()
-            },
-            saveToStore(){
-                localStorage.setItem('VUE-TODOS', JSON.stringify(this.todos))
             }
         }
     }
